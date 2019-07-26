5a5594289a246f76ea2642402539d01069a47879
Khoa Le <khoatle165@gmail.com>
2019-07-26 15:57:19 +0200

    Config set up

---
+++ app.rb
@@ -0,0 +1,17 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+require "pry-byebug"
+require "better_errors"
+configure :development do
+  use BetterErrors::Middleware
+  BetterErrors.application_root = File.expand_path(__dir__)
+end
+
+require_relative "config/application"
+
+set :views, (proc { File.join(root, "app/views") })
+set :bind, '0.0.0.0'
+
+get '/' do
+  erb :index
+end
