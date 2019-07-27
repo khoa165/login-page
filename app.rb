d0f21a017f9690c930934f543bb38641c78e27c2
Khoa Le <khoatle165@gmail.com>
2019-07-27 20:07:18 +0200

    fix style for home page and add an image

--- app.rb
+++ app.rb
@@ -2,9 +2,11 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "pry-byebug"
 require "better_errors"
+
 configure :development do
   use BetterErrors::Middleware
-  BetterErrors.application_root = File.expand_path(__dir__)
+  # BetterErrors.application_root = File.expand_path(__dir__)
+  BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
 require_relative "config/application"
