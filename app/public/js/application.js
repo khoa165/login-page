eadfda4dcbf17aeb156f33a1fca3b07392e6e19a
Khoa Le <khoatle165@gmail.com>
2019-07-30 00:28:46 +0200

    remove js code comment

--- app/public/js/application.js
+++ app/public/js/application.js
@@ -1,14 +1,1 @@
-// function addInput() {
-//   var inputField = document.getElementById('todoInput');
-//   var inputText = inputField.value;
-//   var input = '<h3 class="bg-dark mt-1 rounded text-light">' + inputText + '<h3>';
-//   var myList = document.getElementById('todoList');
-//   var newLi = document.createElement('li');
-//   newLi.innerHTML = input;
-//   myList.appendChild(newLi);
-//   // inputField.value = ""
-// }
 
-// var submitButton = document.getElementById('submitTodoButton');
-
-// // submitButton.addEventListener('click', addInput, false);
